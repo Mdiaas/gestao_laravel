1d1497f9a92c3a346394b522b5a3d39a2ddb9261
gdata-sdh <maugusto.diaas@gmail.com>
2022-03-31 19:21:10 -0300

    atualizações middlewares e views

---
+++ resources/views/app/fornecedor.blade.php
@@ -0,0 +1,6 @@
+@extends('app.layouts.basico')
+
+@section('titulo','Fornecedor')
+@section('conteudo')
+    Fornecedor
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // chamando function principal do PrincipalController
-Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
-
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])
+->name('site.index');
 
 // chamando function sobreNos do SobreNosController
 Route::get('/sobre-nos',[App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
@@ -23,12 +22,15 @@ Route::get('/sobre-nos',[App\Http\Controllers\SobreNosController::class,'sobreNo
 Route::get('/contato', [App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
 Route::post('/contato', [App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
 Route::post('/contato', [App\Http\Controllers\ContatoController::class,'salvar'])->name('site.contato');
-Route::get('/login', function(){return 'login';})->name('site.login');
+Route::get('/login{erro?}',[App\Http\Controllers\LoginController::class,'index'])->name('site.login');
+Route::post('/login',[App\Http\Controllers\LoginController::class,'autenticacao'])->name('site.login');
 
-Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
-    Route::get('/fornecedores', [App\Http\Controllers\FornecedorController::class,'index'])->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+Route::prefix('/app')->middleware('autenticacao:padrao,visitante')->group(function(){
+    Route::get('/home',[App\Http\Controllers\HomeController::class,'index'])->name('app.home');
+    Route::get('/sair',[App\Http\Controllers\LoginController::class,'sair'])->name('app.sair');
+    Route::get('/cliente',[App\Http\Controllers\ClienteController::class,'index'])->name('app.cliente');
+    Route::get('/fornecedor', [App\Http\Controllers\FornecedorController::class,'index'])->name('app.fornecedor');
+    Route::get('/produto',[App\Http\Controllers\ProdutoController::class,'index'])->name('app.produto');
 });
 
 Route::get('/teste/{p1}/{p2}', [App\Http\Controllers\TesteController::class,'teste'])->name('teste');
